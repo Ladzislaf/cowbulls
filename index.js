9f616be8406b1b06caa71ba48bc2da4d494eb6ae
ladzislaf <ladzislaf@gmail.com>
2024-05-18 15:16:32 +0300

    add webhook

--- index.js
+++ index.js
@@ -16,34 +16,45 @@ bot.telegram.setMyCommands([
 	{ command: '/exit', description: 'Выйти из игры' },
 ]);
 
-bot.start(async ctx => {
+bot.start(async (ctx) => {
+	console.log(`User ${ctx.from.id}. [START]`);
 	await ctx.deleteMessage();
 	return ctx.reply('Команда /play - начать игру.');
 });
 
-bot.command('play', async ctx => {
+bot.command('play', async (ctx) => {
 	await ctx.deleteMessage();
-	return ctx.reply('Выберите режим игры - сколько символов хотите отгадывать?', Markup.inlineKeyboard([[Markup.button.callback('3', 'play3'), Markup.button.callback('4', 'play4'), Markup.button.callback('5', 'play5'), Markup.button.callback('6', 'play6')]]));
+	return ctx.reply(
+		'Выберите режим игры - сколько символов хотите отгадывать?',
+		Markup.inlineKeyboard([
+			[
+				Markup.button.callback('3', 'play3'),
+				Markup.button.callback('4', 'play4'),
+				Markup.button.callback('5', 'play5'),
+				Markup.button.callback('6', 'play6'),
+			],
+		])
+	);
 });
 
-bot.command('exit', async ctx => {
+bot.command('exit', async (ctx) => {
 	await ctx.deleteMessage();
 	return ctx.reply('Игра еще не начата.\nКоманда /play - начать игру.');
 });
 
-bot.action('play3', ctx => {
+bot.action('play3', (ctx) => {
 	return playAction(ctx, 3);
 });
 
-bot.action('play4', ctx => {
+bot.action('play4', (ctx) => {
 	return playAction(ctx, 4);
 });
 
-bot.action('play5', ctx => {
+bot.action('play5', (ctx) => {
 	return playAction(ctx, 5);
 });
 
-bot.action('play6', ctx => {
+bot.action('play6', (ctx) => {
 	return playAction(ctx, 6);
 });
 
@@ -54,6 +65,19 @@ async function playAction(ctx, gameMode) {
 	return ctx.answerCbQuery(gameMode);
 }
 
-bot.launch(() => console.log('CowBulls bot is running.'))
+if (process.env.LOCAL_MODE === 'on') {
+	bot.launch(() => console.log('CowBulls bot is running locally.'));
+} else {
+	bot.launch(
+		{
+			webhook: {
+				domain: process.env.DOMAIN,
+				port: process.env.PORT || 443,
+			},
+		},
+		() => console.log('CowBulls bot is running on webhook.')
+	);
+}
+
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
